7206a3dbba83c369dbcb558e56797c3b4ff5946e
Tecnologías de la Información <tic.calama@inacap.cl>
2024-07-03 09:53:58 -0400

    db50

--- src/componente/Inicio.js
+++ src/componente/Inicio.js
@@ -1,8 +1,14 @@
 import React,{useEffect,useState} from "react"
+import { Link } from "react-router-dom"
 
 const Inicio = () =>{
     return(
+        <>
         <h1>hola desde el inicio</h1>
+        <div>
+            <Link to="perfil">ir a perfil con react</Link>
+        </div>
+        </>
     )
 }
 export default Inicio
--- src/componente/Perfil.js
+++ src/componente/Perfil.js
@@ -1,8 +1,17 @@
 import React,{useEffect,useState} from "react"
 
 const Perfil = () =>{
+    //contador de likes
+    const[contadorLikes, setContadorLikes] = useState(0)
+    const handleContador = () =>{
+        setContadorLikes(contadorLikes+1)
+    }
     return(
-        <h1>estoy enperfil</h1>
+        <>
+        <h1>estoy en perfil</h1>
+        <button type="button" onClick={handleContador}> sumar like</button>
+        <p>conteo de likes : {contadorLikes}</p>
+        </>
     )
 }
 export default Perfil
